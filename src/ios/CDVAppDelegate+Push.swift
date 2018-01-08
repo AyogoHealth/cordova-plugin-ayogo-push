7193512cc80a630bf21e825b342a687946749ade
Raul <raul@ayogo.com>
2018-01-08 15:51:35 -0800

    Remove unused code

--- src/ios/CDVAppDelegate+Push.swift
+++ src/ios/CDVAppDelegate+Push.swift
@@ -9,33 +9,8 @@ let CDV_PushPreference      = "CordovaPushPreference";
 let CDV_PushRegistration    = "CordovaPushRegistration";
 
 // Notification Center hooks
-let CordovaDidRegisterUserNotificationSettings = "CordovaDidRegisterUserNotificationSettings";
 let CordovaDidRegisterForRemoteNotificationsWithDeviceToken = "CordovaDidRegisterForRemoteNotificationsWithDeviceToken";
 let CordovaDidFailToRegisterForRemoteNotificationsWithError = "CordovaDidFailToRegisterForRemoteNotificationsWithError";
-
-
-// Keep track of swizzled methods
-private var _CDV_didRegisterUserNotificationSettings        = false;
-private var _CDV_didRegisterForRemoteNotifications          = false;
-private var _CDV_didFailToRegisterForRemoteNotifications    = false;
-private var _CDV_willFinishLaunchingWithOptions             = false;
-
-
-
-func _swizzleMethod(klass : AnyClass, original : Selector, replacement : Selector) -> Bool {
-    let originalMethod = class_getInstanceMethod(klass, original);
-    let swizzledMethod = class_getInstanceMethod(klass, replacement);
-
-    if class_addMethod(klass, original, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod)) {
-        class_replaceMethod(klass, replacement, method_getImplementation(originalMethod), method_getTypeEncoding(swizzledMethod));
-        return false;
-    } else {
-        method_exchangeImplementations(originalMethod, swizzledMethod);
-        return true;
-    }
-}
-
-
 
 extension CDVAppDelegate {
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
