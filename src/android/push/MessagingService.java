76696a1a28f887f627f919d9b27aec89ee7bb9fb
Darryl Pogue <darryl@ayogo.com>
2020-04-27 12:21:47 -0700

    [Android] Handle clicks while app is open (#51)

--- src/android/push/MessagingService.java
+++ src/android/push/MessagingService.java
@@ -43,6 +43,8 @@ public class MessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             sendNotification(remoteMessage);
@@ -51,20 +53,21 @@ public class MessagingService extends FirebaseMessagingService {
 
     private void sendNotification(RemoteMessage remoteMessage) {
         PackageManager pm = getApplicationContext().getPackageManager();
-        Intent launchIntent = pm.getLaunchIntentForPackage(getApplicationContext().getPackageName());
-        String activityClassName = launchIntent != null ? launchIntent.getComponent().getClassName() : null;
 
-        if (activityClassName == null) {
-            Log.e(TAG, "Could not find activity class name");
-            return;
+        Intent launchIntent = pm.getLaunchIntentForPackage(getApplicationContext().getPackageName());
+        launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        launchIntent.setAction("push"); // This is important for the AppScope
+
+        Map<String, String> data = remoteMessage.getData();
+        if (data != null) {
+            Set<String> keys = data.keySet();
+
+            for (String key : keys) {
+                launchIntent.putExtra(key, data.get(key));
+            }
         }
 
-        Intent intent = new Intent();
-        intent.setAction("push"); // This is important for the AppScope
-        intent.setClassName(getApplicationContext(), activityClassName);
-        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, launchIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         ApplicationInfo applicationInfo = null;
         Resources resources = null;
@@ -80,45 +83,36 @@ public class MessagingService extends FirebaseMessagingService {
             return;
         }
 
-        String channelId = applicationInfo.metaData.getString("com.google.firebase.messaging.default_notification_channel_id");
         int iconId = resources.getIdentifier("notification", "drawable", getApplicationContext().getPackageName());
 
         if (iconId == 0) {
             Log.w(TAG, "Could not find the notification icon.");
         }
 
-        if (channelId == null) {
-            channelId = getApplicationContext().getPackageName();
-        }
-
-        /* This will configure a heads-up notification. We need to set the priority as max and also enable the vibration. */
+        /* This will configure a heads-up notification */
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
 
         builder
             .setDefaults(Notification.DEFAULT_ALL)
             .setTicker(remoteMessage.getNotification().getBody())
+            .setContentIntent(pendingIntent)
             .setContentTitle(remoteMessage.getNotification().getTitle())
             .setContentText(remoteMessage.getNotification().getBody())
             .setStyle(new NotificationCompat.BigTextStyle().bigText(remoteMessage.getNotification().getBody()))
-            .setPriority(Notification.PRIORITY_MAX)
-            .setVibrate(new long[]{0, Notification.DEFAULT_VIBRATE})
+            .setPriority(Notification.PRIORITY_HIGH)
             .setSmallIcon(iconId)
             .setAutoCancel(true);
-
-        Map<String, String> data = remoteMessage.getData();
-
-        if (data != null) {
-            Set<String> keys = data.keySet();
-
-            for (String key : keys) {
-                intent.putExtra(key, data.get(key));
-            }
-        }
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         /* Since android Oreo notification channel is needed. */
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = applicationInfo.metaData.getString("com.google.firebase.messaging.default_notification_channel_id");
+
+            if (channelId == null) {
+                channelId = getApplicationContext().getPackageName();
+            }
+
             builder.setChannelId(channelId);
 
             int appNameId = applicationInfo.labelRes;
